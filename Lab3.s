4361d9894e9b21b42015c612479d567da1c28997
BryanTheProgram <42870970+BryanTheProgram@users.noreply.github.com>
2024-10-21 23:32:21 +0200

    Lab3 - code working
    let's begin

--- Lab3.s
+++ Lab3.s
@@ -13,18 +13,17 @@ v3:  .double 1.2,2.2,3.2,4.4,5.0,6.1,1.2,2.2,3.2,4.4,5.0,6.1
 v4: .space 256 ; 32*8 , sapendo che abbiamo 32 elementi ognuno su 64 bit ( 8 byte )
 v5: .space 256
 v6: .space 256
-m: .word 2 ; intero su 64 bit
-a: .space 8 ; elemento double ( su 64 bit )
-b: .double 1.5
+m: .word 1 ; intero su 64 bit
 
-
- 
 
 .text 
 MAIN:
 ori R10,R0,248 ; indice del ciclo
-ori R11,R0,1 ; indice del multiplo 
+ori R11,R0,1 ; indice per calcolare il multiplo 
+ori R28,R0,31 ;indice i
 
+mtc1 R0,F29 ;lo setto a 0
+ld R12,m(R0) ; carico dalla memoria il valore di m
 
 START:
 
@@ -33,13 +32,11 @@ l.d F1,v1(R10)
 l.d F2,v2(R10)
 l.d F3,v3(R10)
 
-ld R12,m(R0) ; carico in memoria il valore di m
-
 IF: 
 
 bnez R11,ELSE
 
-dsllv R13,R12,R10 ;shift logico di m di i posizioni
+dsllv R13,R12,R28 ;shift logico di m di i posizioni
 
 ;trasformo m shiftato in floating point
 mtc1 R13,F11
@@ -51,7 +48,7 @@ div.d F11,F1,F11
 j ENDIF
 
 ELSE:
-dmul R12,R12,R10
+dmul R12,R12,R28
 
 ;trasformo R12 in floating point
 mtc1 R12,F11
@@ -59,18 +56,19 @@ cvt.d.l F11,F11
 
 mul.d F11,F1,F11
 
+
 ENDIF:
+ADD.D F30,F29,F11
+
 ;lo riconverto m in intero
-cvt.l.d F11,F11 
-mfc1 R12,F11
+cvt.l.d F30,F31 
+mfc1 R12,F31
 
-;utilizzeremo F11 come A
-cvt.d.l F11,F11
-
+; F11 è A
 
 bnez R11,NON_AZZERARE
 
-ori R11,R0,3
+ori R11,R0,4
 
 NON_AZZERARE:
 daddi R11,R11,-1
@@ -80,8 +78,7 @@ mul.d F7,F11,F1 ; a * v1
 sub.d F4,F7,F2
 
 ;calcolo v5
-div.d F8,F4,F3
-sub.d F5,F8,F2
+div.d F5,F4,F3
 
 
 ;calcolo v6
@@ -94,7 +91,7 @@ s.d F5,v5(R10)
 s.d F6,v6(R10)
 
 daddi R10,R10,-8
-
+daddi R28,R28,-1
 
 bnez R10,START 
 ;nop
